cce98b99fb8fba748d3ca5364b094e7edaecac18
JakubKub13 <jakubkubala3@gmail.com>
2023-03-20 07:06:54 +0100

    Adding swaps

--- contracts/HyroSwap.sol
+++ contracts/HyroSwap.sol
@@ -30,7 +30,7 @@ contract HyroSwap {
 
 
 // Single Hop Swap V3
-    function hyroSwapUniswapV3(
+    function hyroSwapUniswapV3SingleHopExactAmountIn(
         address _uniswapV3Router,
         address _tokenIn,
         address _tokenOut,
@@ -64,11 +64,44 @@ contract HyroSwap {
         return amountOut;
     }
 
-    function hyroSwapUniswapV3MultiHop() external {}
+    function hyroSwapUniswapV3SingleHopExactAmountOut(
+        address _uniswapV3Router,
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountOutDesired,
+        uint256 _amountInMax
+    ) external {
+        require(_amountOutDesired > 0, "amount must be greater than 0");
+        require(_tokenIn != _tokenOut, "tokenIn and tokenOut must be different");
+
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountInMax);
+        IERC20(_tokenIn).approve(_uniswapV3Router, _amountInMax);
+
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter
+            .ExactOutputSingleParams({
+                tokenIn: _tokenIn,
+                tokenOut: _tokenOut,
+                fee: 3000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: _amountOutDesired,
+                amountInMaximum: _amountInMax,
+                sqrtPriceLimitX96: 0
+            });
+        uint256 amountIn = ISwapRouter(_uniswapV3Router).exactOutputSingle(params);
+
+        if(amountIn < _amountInMax) {
+            IERC20(_tokenIn).approve(_uniswapV3Router, 0);
+            IERC20(_tokenIn).transfer(msg.sender, _amountInMax - amountIn);
+        }
+    }
+
+    //function hyroSwapUniswapV3MultiHop() external {}
 
 
-// Single Hop Swap V2
-    function hyroSwapUniswapV2(
+// UNISWAP V2 -----------------------------------------------------------------------------------------------------------------------------
+
+    function hyroSwapUniswapV2SingleHopExactAmountIn(
         address _uniswapV2Router,
         address _tokenIn,
         address _tokenOut,
@@ -86,11 +119,86 @@ contract HyroSwap {
         path[0] = _tokenIn;
         path[1] = _tokenOut;
 
-        uint256[] memory amounts = IUniswapV2Router02(_uniswapV2Router)
+        IUniswapV2Router02(_uniswapV2Router)
             .swapExactTokensForTokens(_amount, _amountOutMin, path, msg.sender, block.timestamp);
-
-        emit Swap(_tokenIn, _tokenOut, _amount, amounts[1], msg.sender);
     }
 
-    function hyroSwapUniswapV2MultiHop() external {}
+    function hyroSwapUniswapV2SingleHopExactAmountOut(
+        address _uniswapV2Router,
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountOutDesired,
+        uint256 _amountInMax
+    ) external {
+        require(_amountOutDesired > 0, "amount must be greater than 0");
+        require(_tokenIn != _tokenOut, "tokenIn and tokenOut must be different");
+
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountInMax);
+        console.log(IERC20(_tokenIn).balanceOf(address(this)), "Balance of token in");
+        IERC20(_tokenIn).approve(_uniswapV2Router, _amountInMax);
+
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+
+        uint256[] memory amounts = IUniswapV2Router02(_uniswapV2Router)
+            .swapTokensForExactTokens(_amountOutDesired, _amountInMax, path, msg.sender, block.timestamp);
+
+        if(amounts[0] < _amountInMax) {
+            IERC20(_tokenIn).transfer(msg.sender, _amountInMax - amounts[0]);
+        }
+    }
+
+// Swap DAI to WETh to CRV
+    function hyroSwapUniswapV2MultiHopExactAmountIn(
+        address _uniswapV2Router,
+        address _tokenIn,
+        address _pathToken, 
+        address _tokenOut, 
+        uint256 _amountIn,
+        uint256 _amountOutMin
+    ) external {
+        require(_amountIn > 0, "amount must be greater than 0");
+        require(_tokenIn != _pathToken && _tokenIn != address(0), "tokenIn and tokenOut must be different");
+        require(_pathToken != _tokenOut && _pathToken != address(0), "tokenIn and tokenOut must be different");
+        require(_tokenOut != address(0), "tokenOut must be different");
+
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        IERC20(_tokenIn).approve(_uniswapV2Router, _amountIn);
+        address[] memory path = new address[](3);
+        path[0] = _tokenIn;
+        path[1] = _pathToken;
+        path[2] = _tokenOut;
+
+        IUniswapV2Router02(_uniswapV2Router)
+            .swapExactTokensForTokens(_amountIn, _amountOutMin, path, msg.sender, block.timestamp);
+    }
+
+    function hyroSwapUniswapV2MultiHopExactAmountOut(
+        address _uniswapV2Router,
+        address _tokenIn,
+        address _pathToken,
+        address _tokenOut,
+        uint256 _amountOutDesired,
+        uint256 _amountInMax
+    ) external {
+        require(_amountOutDesired > 0, "amount must be greater than 0");
+        require(_tokenIn != _pathToken && _tokenIn != address(0), "tokenIn and tokenOut must be different");
+        require(_pathToken != _tokenOut && _pathToken != address(0), "tokenIn and tokenOut must be different");
+        require(_tokenOut != address(0), "tokenOut must be different");
+
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountInMax);
+        IERC20(_tokenIn).approve(_uniswapV2Router, _amountInMax);
+        address[] memory path = new address[](3);
+        path[0] = _tokenIn;
+        path[1] = _pathToken;
+        path[2] = _tokenOut;
+
+        uint256[] memory amounts = IUniswapV2Router02(_uniswapV2Router)
+            .swapTokensForExactTokens(_amountOutDesired, _amountInMax, path, msg.sender, block.timestamp);
+
+        if(amounts[0] < _amountInMax) {
+            IERC20(_tokenIn).transfer(msg.sender, _amountInMax - amounts[0]);
+        }
+    }
 }
